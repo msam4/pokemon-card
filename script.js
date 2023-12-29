26f33b402b48e70af9346518013520b2b2dd46f7
Michael Sam <sam.michael.h@gmail.com>
2023-12-29 14:44:09 +0900

    Change background color based on pokemon type in js file

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const typeColor = {
+  bug: "#26de81",
+  dragon: "#ffeaa7",
+  electric: "#fed330",
+  fairy: "#FF0069",
+  fighting: "#30336b",
+  fire: "#f0932b",
+  flying: "#81ecec",
+  grass: "#00b894",
+  ground: "#EFB549",
+  ghost: "#a55eea",
+  ice: "#74b9ff",
+  normal: "#95afc0",
+  poison: "#6c5ce7",
+  psychic: "#a29bfe",
+  rock: "#2d3436",
+  water: "#0190FF",
+};
 const url = " https://pokeapi.co/api/v2/pokemon/";
 const card = document.getElementById("card");
 const btn = document.getElementById("btn");
@@ -28,10 +46,15 @@ let generateCard = (data) => {
   console.log(data);
   const hp = data.stats[0].base_stat;
   const imgSrc = data.sprites.other.dream_world.front_default;
-  const pokeName = data.name;
+  const pokeName = data.name[0].toUpperCase() + data.name.slice(1);
   const statAttack = data.stats[1].base_stat;
   const statDefense = data.stats[2].base_stat;
   const statSpeed = data.stats[5].base_stat;
+
+  // Set typeColor based on the pokemon type
+
+  const themeColor = typeColor[data.types[0].type.name];
+  console.log(themeColor);
 
   card.innerHTML = `
       <p class="hp">
@@ -61,6 +84,7 @@ let generateCard = (data) => {
       </div>
   `;
   appendTypes(data.types);
+  styleCard(themeColor);
 };
 
 let appendTypes = (types) => {
@@ -71,6 +95,13 @@ let appendTypes = (types) => {
     // console.log(span);
     document.querySelector(".types").appendChild(span);
   })
+};
+
+let styleCard = (color) => {
+  card.style.background = `radial-gradient(circle at 50% 0%, ${color} 36%, #ffffff 36%)`;
+  card.querySelectorAll(".types span").forEach((typeColor) => {
+    typeColor.style.backgroundColor = color;
+  });
 };
 
 btn.addEventListener("click", getPokeData);
